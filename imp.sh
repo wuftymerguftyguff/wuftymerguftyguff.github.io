bc05b68ee6a610f09fbf9773ca7f3d6d47a7b00e
Jeff Arthur <wuftymerguftyguff@gmail.com>
2016-07-04 21:09:46 +0100

    import of jeffarthur.co.uk to jekyll

---
+++ imp.sh
@@ -0,0 +1,18 @@
+ruby -rubygems -e 'require "jekyll-import";
+    JekyllImport::Importers::WordPress.run({
+      "dbname"   => "WP_jeffarthurcouk",
+      "user"     => "jauser",
+      "password" => "jaass",
+      "host"     => "ws02.local",
+      "socket"   => "",
+      "table_prefix"   => "wp_",
+      "site_prefix"    => "",
+      "clean_entities" => true,
+      "comments"       => true,
+      "categories"     => true,
+      "tags"           => true,
+      "more_excerpt"   => true,
+      "more_anchor"    => true,
+      "extension"      => "html",
+      "status"         => ["publish"]
+    })'
